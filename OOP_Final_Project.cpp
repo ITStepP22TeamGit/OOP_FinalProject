592d20436de487960273b82107b414e4884131dd
Mishanja_Games <62800428+MishanjaGames@users.noreply.github.com>
2024-02-11 20:33:28 +0200

    Init Commit
    test

---
+++ OOP_Final_Project.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+//#include "Room.h"
+//int Room::counter = 0;
+int main()
+{
+    srand(time(0));
+    cout << "Hello World!\n";
+}
+
+//vector<Room*>arr;
+//arr.push_back(new LRoom(5, true));
+//arr.push_back(new SRoom(5, true));
+//arr.push_back(new MRoom(5, true));
+//arr.push_back(new PRoom(5, true));
+//arr.push_back(new HRoom(5, true));
+//arr.push_back(new VRoom(5, true));
+//arr.push_back(new LxRoom(5, true));
+//arr.push_back(new PresRoom(5, true));
+//
+//for (int i = 0; i < arr.size(); i++) {
+//    arr[i]->show();
+//}
+//
+//arr[0]->setOqqupierInfo(60, "Vasilij Pupkin", "", Date(01, 07, 2024));
+//
+//for (int i = 0; i < arr.size(); i++) {
+//    arr[i]->show();
+//}
+
+
